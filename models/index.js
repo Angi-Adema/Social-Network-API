a4881759e0d3881fe0f7ceaa2a631919a4030ad6
CoderGirl <angi.adema@gmail.com>
2022-12-14 11:29:17 -0700

    updated the models folder with model files and completed the model index.js

---
+++ models/index.js
@@ -0,0 +1,6 @@
+//Require the User and Thought models.
+const User = require('./User');
+const Thought = require('./Thought');
+
+//Export User and Thought modules.
+module.exports = { User, Thought };
