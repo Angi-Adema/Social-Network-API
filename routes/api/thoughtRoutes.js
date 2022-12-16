09a7913a7a02aaa86c5f1c86abb6264cbbaf20b6
CoderGirl <angi.adema@gmail.com>
2022-12-15 17:47:13 -0700

    updated the thought and user routes with pseudocode.

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,17 @@
+//Require express router.
+const router = require("express").Router();
+
+//Set up the requirements from the thought controller.
+
+//Set up GET route for all thoughts.
+
+//Set up GET route to get thought by id, PUT route to update thought and DELETE to delete the thought.
+
+//Set up POST route to create a thought.
+
+//Set up a POST route to add a reaction.
+
+//Set up a DELETE route to delete a reaction.
+
+//Export the router.
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+//Require express router.
+const router = require('express').Router();
+
+//Set up requirements from the User controller.
+
+//Set up GET route to get all users and POST route to create a user.
+
+//Set up a GET route to get a user by id, PUT route to update a user and DELETE route to delete a user.
+
+//Set up a POST route to add a friend and a DELETE route to delete a friend.
+
+//Export the router.
+module.exports = router;
