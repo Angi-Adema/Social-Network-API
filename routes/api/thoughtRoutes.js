e3428f4fd220f4f7a577c9f3b060c67996db9bbc
CoderGirl <angi.adema@gmail.com>
2022-12-15 18:06:01 -0700

    added routes pseudo code

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,15 +3,15 @@ const router = require("express").Router();
 
 //Set up the requirements from the thought controller.
 
-//Set up GET route for all thoughts.
+//Set up GET route to get all thoughts.
 
-//Set up GET route to get thought by id, PUT route to update thought and DELETE to delete the thought.
+//Set up GET route to get thought by id, PUT route to update thought by id and DELETE to delete the thought by its id.
 
-//Set up POST route to create a thought.
+//Set up POST route to create a thought (push created thought to id to the associated user's thoughts array field).
 
-//Set up a POST route to add a reaction.
+//Set up a POST route to create a reaction stored in a single thought's reactions array field.
 
-//Set up a DELETE route to delete a reaction.
+//Set up a DELETE route to delete a reaction by its reactionId value.
 
 //Export the router.
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,9 +5,9 @@ const router = require('express').Router();
 
 //Set up GET route to get all users and POST route to create a user.
 
-//Set up a GET route to get a user by id, PUT route to update a user and DELETE route to delete a user.
+//Set up a GET route to get a user by id with populated thought and friend data, PUT route to update a user by id and DELETE route to delete a user by id.
 
-//Set up a POST route to add a friend and a DELETE route to delete a friend.
+//Set up a POST route to add a friend and user friend list and a DELETE route to delete a friend from a user friend list                 .
 
 //Export the router.
 module.exports = router;
