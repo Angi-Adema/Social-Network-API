a0aeb0fbcbabb7d2d813b95426c3831ba03088cc
CoderGirl <angi.adema@gmail.com>
2022-12-15 21:20:47 -0700

    updated thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,20 +2,34 @@
 const { Thought, User } = require('../models');
 
 //Set up thoughts controller.
-
-//Create a new thought.
+module.exports = {
+  //Create a new thought.
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: 'Thought created, but found no user with that ID',
+            })
+          : res.json('Created the application 🎉')
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
 
 //Get all thoughts.
-
 //Get a single thought by id.
-
 //Update a current thought by id.
-
 //Delete a current thought by id.
-
 //Add a new reaction.
-
 //Delete a reaction by id.
-
-//Export the controller.
-module.exports = thoughtController;
