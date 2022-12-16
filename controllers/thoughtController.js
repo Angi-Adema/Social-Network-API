f820aa8faa94cd44c945f4c5d21cf51e50faec9f
CoderGirl <angi.adema@gmail.com>
2022-12-15 18:11:19 -0700

    added pseudo code to thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,21 @@
+//Require the thought and user models.
+const { Thought, User } = require('../models');
+
+//Set up thoughts controller.
+
+//Create a new thought.
+
+//Get all thoughts.
+
+//Get a single thought by id.
+
+//Update a current thought by id.
+
+//Delete a current thought by id.
+
+//Add a new reaction.
+
+//Delete a reaction by id.
+
+//Export the controller.
+module.exports = thoughtController;
