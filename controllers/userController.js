5a35fdc9d4c6d1b9b46ac0a845b3df3e54db854b
CoderGirl <angi.adema@gmail.com>
2022-12-15 18:15:41 -0700

    updated the user controller with pseudo code.

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+//Require the User model.
+const { User } = require("../models");
+
+//Set up users controller.
+
+//Create a new user.
+
+//Get all users.
+
+//Get a single user by id.
+
+//Update a current user by id.
+
+//Delete a current user by id.
+
+//Delete a current friend.
+
+//Export the user controller.
+module.exports = userController;
